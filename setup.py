f103d83528f627c07d57aeae8154af88e9b1dd87
frontedTee <adetunjitayo2019@gmail.com>
2023-05-05 15:05:41 -1200

    fix:fix folder tree

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-import setuptools 
 
-setuptools.setup(
-    name="Generate_readme",
+
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+    name="generate-readme",
     version="0.1",
     description="Generate a README.md file for your projects",
     author="Adetunji Adetayo",
     author_email="<litcoderr@gmail.com>",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url = "https://github.com/Navlos/Generate_Readme_cli.git",
+    py_modules=["generate_readme"],
     keywords=["README", "python"],
     install_requires=["markdown"],
     classifiers=[
